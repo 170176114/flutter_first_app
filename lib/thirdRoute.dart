983471f7c8ab87e4a8f38e5d01520a0cd7768305
vtc170176114 <5061870-170176114@users.noreply.gitlab.com>
2020-06-15 21:42:01 +0800

    next page

---
+++ lib/thirdRoute.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: ThirdRoute(),
+  ));
+}
+
+class ThirdRoute extends StatelessWidget {
+  @override
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Route"),
+      ),
+      body: Center(
+        child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'This is BMI Application',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
